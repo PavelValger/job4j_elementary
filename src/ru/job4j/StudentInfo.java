1fab1fdbddb33686a986c53f47274b23085fe3b7
PavelValger <pavelwalker@mail.ru>
2021-08-31 20:45:47 +0600

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pavel Valger");
+        System.out.println("04.05.1992");
+    }
+}
