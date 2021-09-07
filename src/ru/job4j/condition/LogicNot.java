ba5dff373b9c613350d3d658b0974e68eae71221
PavelValger <pavelwalker@mail.ru>
2021-09-07 07:45:57 +0600

    4.3.3. Логическое отрицание ! [#334072]

--- src/ru/job4j/condition/LogicNot.java
+++ src/ru/job4j/condition/LogicNot.java
@@ -24,20 +24,4 @@ public class LogicNot {
     public static boolean evenOrNotPositive(int num) {
         return isEven(num) || notPositive(num);
     }
-
-    public static void main(String[] args) {
-        int num = -5;
-        boolean result = LogicNot.isEven(num);
-        System.out.println("Число " + num + " четное? " + result);
-        boolean result1 = LogicNot.isPositive(num);
-        System.out.println("Число " + num + " положительное? " + result1);
-        boolean result2 = LogicNot.notEven(num);
-        System.out.println("Число " + num + " нечетное? " + result2);
-        boolean result3 = LogicNot.notPositive(num);
-        System.out.println("Число " + num + " отрицательное? " + result3);
-        boolean result4 = LogicNot.notEvenAndPositive(num);
-        System.out.println("Число " + num + " нечетное и положительное? " + result4);
-        boolean result5 = LogicNot.evenOrNotPositive(num);
-        System.out.println("Число " + num + " четное или отрицательное? " + result5);
-    }
 }
