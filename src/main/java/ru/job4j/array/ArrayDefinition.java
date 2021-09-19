b4cf55e7653f7aecdf3d6547995ba45eeed99206
PavelValger <pavelwalker@mail.ru>
2021-09-19 13:31:20 +0600

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Pavel Valger";
+        names[1] = "Petr Arsentev";
+        names[2] = "Stas Korobeinikov";
+        names[3] = "Elena Kartashova";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
